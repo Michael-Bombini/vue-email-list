3a3e8e265e10a098eae37d096eaa7c8a9eeb6a45
Michael Bombini <bombini.michael@gmail.com>
2022-06-17 14:10:06 +0200

    Generated 10 emails

---
+++ js/main.js
@@ -0,0 +1,35 @@
+// Descrizione:
+// Attraverso l’apposita API di Boolean
+// https://flynn.boolean.careers/exercises/api/random/mail
+// generare 10 indirizzi email e stamparli in pagina all’interno di una lista.
+// Bonus
+// Far comparire gli indirizzi email solamente quando sono stati tutti generati.
+
+
+
+new Vue({
+
+el : '#app',
+
+data : {
+
+    indirizzi : [],
+    
+},
+
+methods : {
+        
+    generateMail : function(){
+        for(let i = 0 ; i < 10 ; i++){
+        axios
+        .get("https://flynn.boolean.careers/exercises/api/random/mail")
+        .then((axiosResp) => {
+          this.indirizzi.push(axiosResp.data.response);
+        });
+
+    }
+    }
+
+},
+
+})
