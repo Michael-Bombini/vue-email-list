c6400f0eeed02388d14e5851e50551a9dfd773d3
Michael Bombini <bombini.michael@gmail.com>
2022-06-17 14:13:55 +0200

    Completed bonus

--- js/main.js
+++ js/main.js
@@ -20,6 +20,7 @@ data : {
 methods : {
         
     generateMail : function(){
+        this.indirizzi = [];
         for(let i = 0 ; i < 10 ; i++){
         axios
         .get("https://flynn.boolean.careers/exercises/api/random/mail")
